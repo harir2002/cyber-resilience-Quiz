dcb66082aaac5f05fa15501e24ecad942f00e734
Hariktm <hariktm05@gmail.com>
2026-01-31 21:12:21 +0530

    Remove legacy Streamlit components to fix startup

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,5 +1,4 @@
 """Utils package initialization"""
 from .scoring import ResilienceScorer
-from .ui_components import apply_custom_css, render_header, render_section_header
 
-__all__ = ['ResilienceScorer', 'apply_custom_css', 'render_header', 'render_section_header']
+__all__ = ['ResilienceScorer']
